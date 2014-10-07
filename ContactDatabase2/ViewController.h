4f5bbc092e9ec71f1ac51e36047b1532629102d4
Chuck Konkol <ckonkol@gmail.com>
2014-10-06 19:08:09 -0500

    Week 8 InClass Contact DB

---
+++ ContactDatabase2/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ContactDatabase2
+//
+//  Created by Charles Konkol on 10/6/14.
+//  Copyright (c) 2014 Rock Valley College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContactDatabase2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ContactDatabase2
+//
+//  Created by Charles Konkol on 10/6/14.
+//  Copyright (c) 2014 Rock Valley College. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
